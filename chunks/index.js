fecc44e4990f41d396e6e3d3a6224aad46084647
Olha Danylchenko <olhadanylchenko@air-olha.localdomain>
2019-11-24 13:41:49 +0100

    adding input value to the ul

--- chunks/index.js
+++ chunks/index.js
@@ -6,10 +6,18 @@ function addIngredient(e) {
     document.getElementById("form").reset();
     if (value.trim() != "") {
         ingredients.push(value);
-        console.log(ingredients)
-    }
+        let ul = document.getElementById("list");
+        let ingredient = document.createTextNode(value);
+        var li = document.createElement("li");
+        li.appendChild(ingredient);
+        ul.appendChild(li);
+    };
 }
 
 document.getElementById('form').addEventListener('submit', addIngredient);
 
+document.getElementById("loggingBtn").addEventListener("click", () => {
+    console.log(ingredients);
 
+
+})
