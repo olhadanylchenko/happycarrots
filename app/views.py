71eb3e3af44888998d8e3dbce8bede78da98cc49
Olha Danylchenko <olhadanylchenko@MacBook-Air-Olha.local>
2019-12-01 23:00:00 +0100

    looks ok

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template,  url_for, request
 from app import app
-
+import urllib.parse
 import requests, json
 
 
@@ -9,21 +9,24 @@ import requests, json
 def index():
     title="Happy Carrots"
     return render_template('index.html', title=title)
-    
+
 
 @app.route('/search')
 def search():
     ingredients = request.args.get("ingredients")
-    url = "https://api.edamam.com/search?q=vegan,%s&app_id=25111ad3&app_key=32b6013efa9b099aaebe182d8ca76f36" %ingredients
+    url = "https://api.edamam.com/search?q=vegan,%s&app_id=25111ad3&app_key=32b6013efa9b099aaebe182d8ca76f36" % ingredients
     response = requests.request("GET", url)
-    image=response.json()['hits'][0]['recipe']['image']
     title = "Happy Carrots Search"
-    return render_template('search.html', title=title, ingredients=ingredients, response=response.json(), image=image)
-    
- 
+    get_uri = urllib.parse.quote
+    return render_template('search.html', title=title, ingredients=ingredients, response=response.json(), get_uri=get_uri)
+
+
 @app.route('/recipe')
 def about():
+    recipe = request.args.get("recipe")
+    url = "https://api.edamam.com/search?r=%s&app_id=25111ad3&app_key=32b6013efa9b099aaebe182d8ca76f36" % urllib.parse.quote(recipe)
+    response = requests.get(url).json()
     title = "Recipe"
-
-    return render_template('recipe.html', title=title)
+    bodyclass = "recipe"
+    return render_template('recipe.html', title=title, response=response, bodyclass=bodyclass)
  
